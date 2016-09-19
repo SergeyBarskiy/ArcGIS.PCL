e0b2d693e58903033e75ce61947cb2986ff3f25b
Sergey Barskiy <SergeyBarskiyTyler@outlook.com>
2016-09-19 17:26:15 -0400

    Remove default spatial reference

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Reflection;
 [assembly: AssemblyCompanyAttribute("")]
 [assembly: AssemblyCopyrightAttribute("Copyright Dave Timmins (c) 2013")]
 [assembly: AssemblyProductAttribute("ArcGIS REST API ServiceModel PCL")]
-[assembly: AssemblyVersionAttribute("5.6.2")]
-[assembly: AssemblyFileVersionAttribute("5.6.2")]
-[assembly: AssemblyInformationalVersionAttribute("5.6.2")]
+[assembly: AssemblyVersionAttribute("5.6.4")]
+[assembly: AssemblyFileVersionAttribute("5.6.4")]
+[assembly: AssemblyInformationalVersionAttribute("5.6.4")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "5.6.1";
-        internal const string InformationalVersion = "5.6.1";
+        internal const string Version = "5.6.4";
+        internal const string InformationalVersion = "5.6.4";
     }
 }
